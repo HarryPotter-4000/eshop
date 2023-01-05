6313a94cefb3b1ee333742c08c215ea537dc56c9
KatePrakofyeva <katajina@icloud.com>
2023-01-05 23:02:22 +0100

    Add Modal WIndow with Portal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
-import { Container, Stack } from '@mui/system';
+import { Box, Button, Container, Stack } from '@mui/material';
+import { useState } from 'react';
 import ProductList from '../components/ProductList';
+import ModalWindow from '../components/ModalWindow';
+import CasinoIcon from '@mui/icons-material/Casino';
 
 function Home(props) {
   const { products, addToOrder } = props;
+  const [openp, setOpenP] = useState(false);
 
   return (
     <Container width="lg" style={{ padding: '0px' }}>
+      <Box sx={{ marginLeft: '24px' }}>
+        <Button
+          size="large"
+          startIcon={<CasinoIcon />}
+          onClick={() => setOpenP(true)}
+        >
+          Maby, I want to buy...
+        </Button>
+        <ModalWindow
+          message="Here you go!"
+          isOpen={openp}
+          onClose={() => setOpenP(false)}
+          products={products}
+        />
+      </Box>
       <Stack
         sx={{
           width: {
